1401502ac8569d41241d09982c5b59fe63d1ae36
compilelife <505610414@qq.com>
2019-12-31 21:18:44 +0800

    添加chunnked示例

---
+++ stream/reader.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "../src/imediareader.h"
+
+using namespace std;
+using namespace httplib;
+
+class MediaReader : IMediaReader{
+    FILE* mFp;
+public:
+    bool open(const httplib::Request& req, 
+                    const std::string& path, 
+                    int64_t from, 
+                    int64_t to) {
+        mFp = fopen(path.c_str(), "rb");
+        return mFp != nullptr;
+    }
+
+    void read(int64_t offset, 
+            std::function<void(const char *data, size_t data_len)> dataSink,
+            std::function<void(int info)> notify) {
+        const int bufSize = 4096;
+        char buf[bufSize];
+
+        auto ret = fread(buf, 1, bufSize, mFp);
+        if (ret < 0)
+            notify(READER_ERROR);
+        else{
+            if (ret > 0)
+                dataSink(buf, ret);
+            if (ret < bufSize)
+                notify(READER_EOF);
+        }
+    }
+
+    int64_t size() {
+        return -1;
+    }
+
+    void close(){
+        fclose(mFp);
+    }
+};
